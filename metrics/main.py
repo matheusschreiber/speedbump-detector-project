1372884214017b9faea8f2702e574748bae2c59f
Matheus <mt.schreiber01@gmail.com>
2023-03-23 18:32:35 -0300

    update on metrics system

---
+++ metrics/main.py
@@ -0,0 +1,16 @@
+import json
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+
+# load the ground truth annotations and predictions
+annFile = 'anno_coco_test.json' # ground truth annotations
+cocoGt = COCO(annFile)
+
+predFile = 'pred_coco_test.json' # model predictions
+cocoDt = cocoGt.loadRes(predFile)
+
+# evaluate the model using COCOeval
+cocoEval = COCOeval(cocoGt, cocoDt, 'bbox')
+cocoEval.evaluate()
+cocoEval.accumulate()
+cocoEval.summarize()
