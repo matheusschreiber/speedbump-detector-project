1f1f3a6b6abe4804240037839af588337340d5d5
Matheus <mt.schreiber01@gmail.com>
2023-09-22 01:39:54 -0300

    refactor code

--- utils/plot graph/main.py
+++ utils/plot graph/main.py
@@ -26,6 +26,8 @@ import seaborn as sns
 sns.set_style('white')
 sns.set_context('poster')
 
+IOUTHRESHOLD=.5
+
 COLORS = [
     '#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
     '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5',
@@ -262,17 +264,18 @@ def plot_pr_curve(
     """Simple plotting helper function"""
 
     if ax is None:
-        plt.figure(figsize=(10,8))
+        plt.figure(figsize=(8,5))
         ax = plt.gca()
 
     if color is None:
         color = COLORS[0]
     ax.scatter(recalls, precisions, label=label, s=20, color=color)
-    ax.set_xlabel('recall')
-    ax.set_ylabel('precision')
-    ax.set_title('Precision-Recall curve for {}'.format(category))
-    ax.set_xlim([0.0,1.3])
-    ax.set_ylim([0.0,1.2])
+    ax.set_xlabel('recall', fontsize=18)
+    ax.set_ylabel('precision', fontsize=18)
+    ax.set_title('Precision-Recall curve for {}'.format(category), fontsize=18)
+    ax.set_xlim([0.8,1.5])
+    ax.set_ylim([0.8,1.1])
+    
     return ax
 
 def convert_jsons(anno_path, pred_path):
@@ -362,15 +365,24 @@ if __name__ == "__main__":
     ax = None
     avg_precs = []
     iou_thrs = []
-    for idx, iou_thr in enumerate(np.linspace(0.5, 0.95, 10)):
-        data = get_avg_precision_at_iou(gt_boxes, pred_boxes, iou_thr=iou_thr)
-        avg_precs.append(data['avg_prec'])
-        iou_thrs.append(iou_thr)
+    # for idx, iou_thr in enumerate(np.linspace(0.5, 0.95, 10)):
+    #     data = get_avg_precision_at_iou(gt_boxes, pred_boxes, iou_thr=iou_thr)
+    #     avg_precs.append(data['avg_prec'])
+    #     iou_thrs.append(iou_thr)
 
-        precisions = data['precisions']
-        recalls = data['recalls']
-        ax = plot_pr_curve(
-            precisions, recalls, label='{:.2f}'.format(iou_thr), color=COLORS[idx*2], ax=ax)
+    #     precisions = data['precisions']
+    #     recalls = data['recalls']
+    #     ax = plot_pr_curve(
+    #         precisions, recalls, label='{:.2f}'.format(iou_thr), color=COLORS[idx*2], ax=ax)
+
+    data = get_avg_precision_at_iou(gt_boxes, pred_boxes, iou_thr=IOUTHRESHOLD)
+    avg_precs.append(data['avg_prec'])
+    iou_thrs.append(IOUTHRESHOLD)
+
+    precisions = data['precisions']
+    recalls = data['recalls']
+    ax = plot_pr_curve(
+        precisions, recalls, label='{:.2f}'.format(IOUTHRESHOLD), color=COLORS[0*2], ax=ax)
 
     # prettify for printing:
     avg_precs = [float('{:.4f}'.format(ap)) for ap in avg_precs]
@@ -378,9 +390,11 @@ if __name__ == "__main__":
     print('map: {:.2f}'.format(100*np.mean(avg_precs)))
     print('avg precs: ', avg_precs)
     print('iou_thrs:  ', iou_thrs)
-    plt.legend(loc='upper right', title='IOU Thr', frameon=True)
-    for xval in np.linspace(0.0, 1.0, 11):
-        plt.vlines(xval, 0.0, 1.1, color='gray', alpha=0.3, linestyles='dashed')
+    
+    plt.legend(loc='lower left', title='IOU Thr', frameon=True)
+    # for xval in np.linspace(0.0, 1.5, 100):
+    #     plt.vlines(xval, 0.0, 1.2, color='gray', alpha=0.2, linestyles='dashed')
     end_time = time.time()
+    plt.tick_params(axis='both', labelsize=17)
     print('\nPlotting and calculating mAP takes {:.4f} secs'.format(end_time - start_time))
     plt.show()
