e851bc30ac49f953a53f3be45ec2562d4197fdc2
Matheus <mt.schreiber01@gmail.com>
2023-10-11 22:04:05 -0300

    removing sklearn

--- metrics/sklearn.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from sklearn.metrics import average_precision_score
-y_true = np.array([1])
-y_scores = np.array([0.01])
-
-
-# first settings
-
-print(average_precision_score(y_true, y_scores))
